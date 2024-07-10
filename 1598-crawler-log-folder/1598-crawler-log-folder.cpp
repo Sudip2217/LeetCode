aceecfbbd2f19a770687272a101e83cef7cd98d3
Sudip Mondal <155430022+Sudip2217@users.noreply.github.com>
2024-07-10 12:27:36 +0530

    Time: 4 ms (67.13%), Space: 14 MB (61.11%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int i, count = 0;
+        for(i = 0; i < logs.size(); i++)
+        {
+            if(logs[i].size() == 3 and logs[i][0] == '.')
+            {
+                if(count > 0)
+                    count--;
+            }
+            else if(logs[i][0] != '.')
+                count++;
+        }
+        return count;
+    }
+};
